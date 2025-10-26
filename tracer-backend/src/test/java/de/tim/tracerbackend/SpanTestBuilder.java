188c0c3671f87f06ef14639c8c435b0d0b19818d
timHa91 <tim.hartmann.91@web.de>
2025-10-26 16:24:20 +0100

    test(TraceTree): add TraceTreeTest
    - Tests for building the Tree and generating the Tree-String
    - SpanTestBuilder for generating span test data

---
+++ tracer-backend/src/test/java/de/tim/tracerbackend/SpanTestBuilder.java
@@ -0,0 +1,49 @@
+package de.tim.tracerbackend;
+
+import de.tim.tracerbackend.model.Span;
+
+import java.time.Instant;
+
+public class SpanTestBuilder {
+
+    private String id = "test-id";
+    private String traceId = "test-trace-id";
+    private String parentId = "test-parent-id";
+    private String serviceName = "test-service-name";
+    private String operation = "test-operation";
+    private int statusCode = 200;
+    private Instant timestamp = Instant.now();
+    private long duration = 1L;
+    private String errorMsg = "test-error-msg";
+    private String type = "test-type";
+
+    public SpanTestBuilder id(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public SpanTestBuilder traceId(String traceId) {
+        this.traceId = traceId;
+        return this;
+    }
+
+    public SpanTestBuilder timestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+        return this;
+    }
+
+    public Span build() {
+        return new Span(
+                id,
+                traceId,
+                parentId,
+                serviceName,
+                operation,
+                statusCode,
+                timestamp,
+                duration,
+                errorMsg,
+                type
+        );
+    }
+}
